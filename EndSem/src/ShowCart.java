ba3a025bfeff6ca3a3b14ae5aed75d129deaa77c
Satadru-Banerjee <41904095+Satadru-Banerjee@users.noreply.github.com>
2020-07-02 00:00:42 +0530

    Patch 1.01

--- EndSem/src/ShowCart.java
+++ EndSem/src/ShowCart.java
@@ -16,22 +16,5 @@ ppublic class ShowCart extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         out.print("<html><head>");
-        out.print("<link rel='stylesheet' href='style.css'>");
-        out.print("<link rel='icon' href='images/logo.png'>");        
-        out.print("</head><body>");
-        out.print("<center><img src='images/logo.png' width='75'>");
-        List<Participant> list = Dao.getAllParticipants();
-        int i=1;
-        out.print("<table border='1' width='80%' id='cus_table2'>");
-        out.print("<caption><h1>Registered Participants List</h1></caption>");
-        out.print("<tr><th>SL</th><th>Name</th><th>Mobile</th><th>EMail</th><th>Occupation</th><th>Affiliation</th><th>Edit</th><th>Delete</th></tr>");
-        for (Participant p : list) {
-            out.print("<tr><td>" + i + "</td><td>" + p.getName() + "</td><td>" + p.getMobile() + "</td><td>"
-                    + p.getEmail() + "</td><td>" + p.getOccupation() + "</td><td>" + p.getAffiliation() + "</td><td><a href='Edit?id="+ p.getId()+"'>Edit</a>"+"</td><td><a href='Delete?id="+p.getId()+"'>Delete</a> "+"</td></tr>");
-                    i++;
-        }
-        out.print("<tr><td colspan=8 align='center'> <a href='registration.html'>Add New Participants</a></td>");
-        out.print("</table></center></body>");         
-        out.close();
-    }
+        
 }    
